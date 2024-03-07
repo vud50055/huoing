d4dabfc8badaf86512c11edf83b035dbd1ddec5c
mylove <duong200401@gmail.com>
2024-03-07 23:54:18 +0700

    duong

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'bạn';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng bạn ';
+const contentCard = 'chúc bạn hương 8/3 vui vẻ thành côngtrong cuộc sống';
+
+const giftImage = '426197010_3690072681265103_4490205899252929177_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
